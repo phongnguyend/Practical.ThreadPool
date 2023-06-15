6ad98b9424d6abe07d65d6f83873f8a38a440219
Phong Nguyen <nguyendoanphong.it@gmail.com>
2023-06-15 12:06:01 +0700

    code init

---
+++ src/Practical.ThreadPool/Practical.ThreadPool/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Practical.ThreadPool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var threadPool = new ThreadPool(3);
+
+            for (int i = 0; i < 200; i++)
+            {
+                int taskNumber = i;
+                threadPool.Execute(() =>
+                {
+                    var msg = $"Thread: {Thread.CurrentThread.ManagedThreadId} executing Task: {taskNumber}";
+                    Console.WriteLine(msg);
+                    Thread.Sleep(1000);
+                });
+            }
+
+            Console.WriteLine("Press any key to stop...");
+            Console.ReadLine();
+
+            threadPool.Stop();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ src/Practical.ThreadPool/Practical.ThreadPool/ThreadPool.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Concurrent;
+
+namespace Practical.ThreadPool
+{
+    internal class ThreadPool
+    {
+        private ConcurrentQueue<Action> _queue = new ConcurrentQueue<Action>();
+        private List<Thread> _threads = new List<Thread>();
+        private bool _stopped = false;
+
+        public ThreadPool(int threadCount)
+        {
+            for (int i = 0; i < threadCount; i++)
+            {
+                var thread = new Thread(DoWork);
+                thread.Start();
+                _threads.Add(thread);
+            }
+        }
+
+        public void Execute(Action action)
+        {
+            _queue.Enqueue(action);
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Thread Pool is stopping...");
+            _stopped = true;
+            foreach (var thread in _threads)
+            {
+                thread.Interrupt();
+            }
+        }
+
+        public int QueueLength => _queue.Count;
+
+        private void DoWork()
+        {
+            while (!_stopped)
+            {
+                try
+                {
+                    if (_queue.TryDequeue(out var action))
+                    {
+                        action();
+                    }
+                }
+                catch (ThreadInterruptedException)
+                {
+                    var msg = $"Thread: {Thread.CurrentThread.ManagedThreadId} is stopping...";
+                    Console.WriteLine(msg);
+                }
+            }
+        }
+    }
+}
